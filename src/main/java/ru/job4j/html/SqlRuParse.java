80df74dfbde94c0e6e0db0c91941457a9e9ec11b
ArtemShDev <dev.art.balt@gmail.com>
2021-10-23 20:09:11 +0300

    4. Схема таблицы Post [#1731 #219746]

--- src/main/java/ru/job4j/html/SqlRuParse.java
+++ src/main/java/ru/job4j/html/SqlRuParse.java
@@ -8,17 +8,9 @@ import ru.job4j.grabber.Parse;
 import ru.job4j.grabber.post.Post;
 import ru.job4j.grabber.utils.DateTimeParser;
 import ru.job4j.grabber.utils.SqlRuDateTimeParser;
-import ru.job4j.quartz.AlertRabbit;
-
-import java.io.IOException;
-import java.io.InputStream;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.Statement;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Properties;
 
 public class SqlRuParse implements Parse {
 
@@ -28,38 +20,8 @@ public class SqlRuParse implements Parse {
         this.dateTimeParser = dateTimeParser;
     }
 
-    public void createTable() {
-        try (Statement statement = getConnection(readProperties()).createStatement()) {
-            statement.execute(String
-                    .format("create table if not exists post%s",
-                            "(id serial primary key, name text, text text, link text, created timestamp)"));
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    private Connection getConnection(Properties cfg) throws Exception {
-        Class.forName(cfg.getProperty("driver-class-name"));
-        return DriverManager.getConnection(
-                cfg.getProperty("url"),
-                cfg.getProperty("username"),
-                cfg.getProperty("password"));
-    }
-
-    private Properties readProperties() {
-        Properties cfg = new Properties();
-        try (InputStream in = AlertRabbit.class.getClassLoader()
-                .getResourceAsStream("rabbit.properties")) {
-            cfg.load(in);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return cfg;
-    }
-
     public static void main(String[] args) {
         SqlRuParse srp = new SqlRuParse(new SqlRuDateTimeParser());
-        srp.createTable();
         List<Post> list = srp.list("https://www.sql.ru/forum/job-offers");
         System.out.println(list);
     }
@@ -68,7 +30,7 @@ public class SqlRuParse implements Parse {
     public List<Post> list(String link) {
         List<Post> lp = new ArrayList<>();
         try {
-            Document doc = Jsoup.connect(String.format(link)).get();
+            Document doc = Jsoup.connect(link).get();
             Elements row = doc.select(".postslisttopic");
             for (Element td : row) {
                 Post post = detail(td.child(0).attr("href"));
