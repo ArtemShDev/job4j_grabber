e41e9434ea779fd7d62034153abe310d3ee69f56
ArtemShDev <dev.art.balt@gmail.com>
2021-10-29 21:06:00 +0300

    1. Реализация кеша на SoftReference [#1592 #222264]

--- src/main/java/ru/job4j/cache/ConsoleInput.java
+++ src/main/java/ru/job4j/cache/ConsoleInput.java
@@ -3,6 +3,7 @@ package ru.job4j.cache;
 import java.util.Scanner;
 
 public class ConsoleInput {
+
     private Scanner scanner = new Scanner(System.in);
 
     public String askStr(String question) {
