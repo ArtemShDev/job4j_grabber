b0832c62773492bc611527196948cf66541f6776
ArtemShDev <dev.art.balt@gmail.com>
2021-11-01 19:53:18 +0300

    2. Что такое TDD? [#4918]

---
+++ src/main/java/ru/job4j/tdd/AccountCinema.java
@@ -0,0 +1,8 @@
+package ru.job4j.tdd;
+
+public class AccountCinema implements Account {
+    @Override
+    public Object getAccTickets() {
+        return null;
+    }
+}
---
+++ src/test/java/ru/job4j/tdd/CinemaTest.java
@@ -0,0 +1,74 @@
+package ru.job4j.tdd;
+
+import static org.hamcrest.Matchers.nullValue;
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+import org.hamcrest.Matcher.*;
+import org.junit.Ignore;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.Calendar;
+import java.util.List;
+import java.util.NoSuchElementException;
+
+public class CinemaTest {
+
+    @Ignore
+    @Test
+    public void buy() {
+        Account account = new AccountCinema();
+        Cinema cinema = new Cinema3D();
+        Calendar date = Calendar.getInstance();
+        date.set(2020, 10, 10, 23, 00);
+        Ticket ticket = cinema.buy(account, 1, 1, date);
+        assertThat(ticket, is(new Ticket3D()));
+    }
+
+    @Ignore
+    @Test
+    public void tryBuyWhenSeatBusy() {
+        Account account = new AccountCinema();
+        Cinema cinema = new Cinema3D();
+        Calendar date = Calendar.getInstance();
+        date.set(2020, 10, 10, 23, 00);
+        Ticket ticket = cinema.buy(account, 1, 1, date);
+        Ticket ticket2 = cinema.buy(account, 1, 1, date);
+        assertThat(ticket2, is(nullValue()));
+    }
+
+    @Ignore
+    @Test(expected = NoSuchElementException.class)
+    public void tryBuyWhenSeatsOutOfRange() {
+        Account account = new AccountCinema();
+        Cinema cinema = new Cinema3D();
+        Calendar date = Calendar.getInstance();
+        date.set(2020, 10, 10, 23, 00);
+        Ticket ticket = cinema.buy(account, 10000, 800, date);
+    }
+
+    @Ignore
+    @Test
+    public void accountHaseManyTickets() {
+        Account account = new AccountCinema();
+        Cinema cinema = new Cinema3D();
+        Calendar date = Calendar.getInstance();
+        date.set(2020, 10, 10, 21, 00);
+        date.set(2020, 10, 17, 23, 00);
+        Ticket ticket = cinema.buy(account, 1, 1, date);
+        Ticket ticket2 = cinema.buy(account, 1, 1, date);
+        assertThat(Arrays.asList(ticket, ticket2), is(account.getAccTickets()));
+    }
+
+    @Ignore
+    @Test
+    public void find() {
+        Cinema cinema = new Cinema3D();
+        cinema.add(new Session3D());
+        List<Session> sessions = cinema.find(session -> true);
+        assertThat(sessions, is(Arrays.asList(new Session3D())));
+    }
+
+
+}
